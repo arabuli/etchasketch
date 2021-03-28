e4ea7b9bf27b038ba44fa8e5d76d10818b960f2d
arabuli <arabulibeqa@gmail.com>
2021-03-28 20:14:44 +0200

    draw with randomrgba instead of black

--- script.js
+++ script.js
@@ -45,11 +45,18 @@ function itemColor() {
   var cls = document.querySelectorAll('.item');
   Array.from(cls).forEach(function (v) {
     v.addEventListener("mouseenter", function () {
-      this.style.background = "black";
+      this.style.background = random_rgba();
     });
   });
 }
 
+
+function random_rgba() {
+  var o = Math.round, r = Math.random, s = 255;
+  return 'rgba(' + o(r()*s) + ',' + o(r()*s) + ',' + o(r()*s) + ',' + r().toFixed(1) + ')';
+}
+
+var color = random_rgba();
 
 
 
